0ab29683c41e139eb2adc15b86e8548c2060178e
Teja Munikrishna Geddam <teja8geddam99@gmail.com>
2026-01-09 21:05:06 +0530

    Fix CORS preflight headers for Express 5

--- server/index.js
+++ server/index.js
@@ -29,6 +29,18 @@ app.options("*", cors());
 
 app.use(express.json());
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://teja-munikrishna-geddam.github.io");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 /* ✅ ROUTES (THIS WAS MISSING) */
 app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
